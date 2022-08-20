2d7eab9a1d0f256fee42ad1ff5f187f2b2ca1f4c
Anastasia <kulevich05@gmail.com>
2022-08-20 23:08:04 +0300

    Arguments of several functions changed.
    - collide_particle2particle()
    - move_particles()

--- src/particles.cpp
+++ src/particles.cpp
@@ -30,11 +30,10 @@ static void swap_velocity(particle_t *particle1, particle_t *particle2)
 }
 
 // Detects and manages collisions between balls.
-static void collide_particle2particle(particle_t *particles, sf::Time *elapsed,
+static void collide_particle2particle(particle_t *particles, sf::Time elapsed,
                                       int i, int count, int num_of_particles)
 {
         assert(particles);
-        assert(elapsed);
 
         static int num_of_collisions = 0;
 
@@ -45,8 +44,8 @@ static void collide_particle2particle(particle_t *particles, sf::Time *elapsed,
                 swap_velocity(&particles[count], &particles[i]);
 
                 sf::Vector2f position = particles[i].ball.getPosition();
-                particles[i].ball.setPosition(position.x + particles[i].velocity.x * elapsed->asSeconds(),
-                                              position.y + particles[i].velocity.y * elapsed->asSeconds());
+                particles[i].ball.setPosition(position.x + particles[i].velocity.x * elapsed.asSeconds(),
+                                              position.y + particles[i].velocity.y * elapsed.asSeconds());
                 }
 }
 
@@ -74,10 +73,9 @@ static void collide_particle2walls(particle_t *particles, sf::Vector2f *pos)
         }
 }
 
-void move_particles(particle_t *particles, sf::Time *elapsed, int num_of_particles)
+void move_particles(particle_t *particles, sf::Time elapsed, int num_of_particles)
 {
         assert(particles);
-        assert(elapsed);
 
         for (int count = 0; count < num_of_particles; count++) {
                 sf::Vector2f pos = particles[count].ball.getPosition();
@@ -87,8 +85,8 @@ void move_particles(particle_t *particles, sf::Time *elapsed, int num_of_particl
                 for (int i = count + 1; i < num_of_particles; i++)
                         collide_particle2particle(particles, elapsed, i, count, num_of_particles);
 
-                pos.x = pos.x + particles[count].velocity.x * elapsed->asSeconds();
-                pos.y = pos.y + particles[count].velocity.y * elapsed->asSeconds();
+                pos.x = pos.x + particles[count].velocity.x * elapsed.asSeconds();
+                pos.y = pos.y + particles[count].velocity.y * elapsed.asSeconds();
 
                 particles[count].ball.setPosition(pos);
         }
